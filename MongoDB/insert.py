7808dd6eafae85904a603c6b91456a09a9afa115
easyesplat <ericchoirr38@gmail.com>
2024-04-22 22:30:55 -0700

    Fully Hurricane Harvey Update

--- MongoDB/insert.py
+++ MongoDB/insert.py
@@ -1,23 +1,20 @@
 # Get the database using the method we defined in pymongo_test_insert file
 from get_database import get_database
 
-# Tester Hurricane Harvey Code
-def process_hurricane_harvey(building_specific_map: dict, hazard_effect_map: dict) -> None:
-    building_specific_doc_id = insert_to_collection("buildings", building_specific_map)
-    hazard_effect_doc_id = insert_to_collection("hazard_effect", hazard_effect_map)
+def process_hurricane_harvey(building_specific_map: dict, hazard_effect_map: dict, db) -> None:
+    building_specific_doc_id = insert_to_collection("buildings", building_specific_map, db)
+    hazard_effect_doc_id = insert_to_collection("hazard_effect", hazard_effect_map, db)
+    return hazard_effect_doc_id, building_specific_doc_id
 
-
-# TODO(@easyesplat): Pull get_database() out of insert_to_collection() for efficiency-reasons.
 '''
 Inserts a document to a specified collection.
 Returns the document id for cross-referencing.
 '''
-def insert_to_collection(collection_name: str, document_map: dict) -> str:
-    db = get_database("hazards")
+def insert_to_collection(collection_name: str, document_map: dict, db) -> str:
     collection = db[collection_name]
     result = collection.insert_one(document_map)
     return result.inserted_id
   
-if __name__ == "__main__":   
+# if __name__ == "__main__":   
     # Get the database
-    insert_to_collection()
+    # insert_to_collection()
--- MongoDB/parsing.py
+++ MongoDB/parsing.py
@@ -3,72 +3,68 @@ import math
 import re
 from insert import process_hurricane_harvey
 
-def parse_hurricane_michael_csv_file(input_file_name: str):
-    data = pd.read_csv(input_file_name)
-    rows = len(data.index)
-    for i in range(1):
-        row = pd.Series(data.iloc[i]).to_dict()
-        hazard_effect_document = {
-            "damage_state": row["status"],
-            "date": row["date"],
-            "assessment_type": row["assessment_type"],
-            "photo": row["all_photos"],
-            "photo_caption": row["all_photos_caption"],
-            "surveyor_notes": collect_notes([row["general_notes"], row["overall_damage_notes"],
-                                            row["structural_notes"], row["wind_damage_details"],
-                                            row["water_induced_damage_notes"]]),
-            "hazards_present": split_to_array_comma(row["hazards_present"]),
-            "wind_damage_rating": row["wind_damage_rating"],
-            "surge_damage_rating": row["surge_damage_rating"],
-            "rainwater_ingress_damage_rating": row["rainwater_ingress_damage_rating"],
-            "roof_structure_damage": row["roof_structure_damage_"],
-            "roof_substrate_damage": row["roof_substrate_damage"],
-            "roof_cover_damage": row["roof_cover_damage_"],
-            "wall_structure_damage": row["wall_structure_damage_"],
-            "wall_substrate_damage": row["wall_substrate_damage_"],
-            "building_envelope_damage": row["building_envelope_damage_"],
-            "wall_fenestration_damage": compute_average_value(row["front_wall_fenestration_damage"], row["left_wall_fenestration_damage"],
-                                                              row["right_wall_fenestration_damage"], row["back_wall_fenestration_damage"]),
-            "soffit_damage": row["soffit_damage"],
-            "fascia_damage": row["fascia_damage_"],
-            "stories_with_damage": row["stories_with_damage"],
-            "piles_missing_or_collapsed": row["_piles_missing_or_collapsed"],
-            "piles_leaning_or_broken": row["_piles_leaning_or_broken"],
-            "cause_of_foundation_damage": row["cause_of_foundation_damage"]
-        }
-        housing_document = {
-            "latitude": row['latitude'],
-            "longitude": row["longitude"],
-            "building_address": row["address_full"],
-            "building_type": row["building_type"],
-            "number_of_stories": row["number_of_stories"],
-            "understory_pct_of_building_footprint": row["understory_pct_of_building_footprint"],
-            "elevation_height": row["first_floor_elevation_feet"],
-            "year_built": int(row["year_built"]),
-            "roof_shape": row["roof_shape"],
-            "roof_system": split_to_array(row["roof_system"]),
-            "roof_to_wall_attachment": row["r2wall_attachment"],
-            "roof_substrate_type": row["roof_substrate_type"],
-            "roof_cover": split_to_array(row["roof_cover"]),
-            "roof_slope": row["roof_slope"],
-            "main_wind_force_resisting_system": split_to_array(row["mwfrs"]),
-            "foundation_type": row["foundation_type"],
-            "wall_anchorage_type": row["wall_anchorage_type"],
-            "wall_structure": row["wall_structure"],
-            "wall_substrate": row["wall_substrate"],
-            "wall_cladding": row["wall_cladding"],
-            "soffit_type": row["soffit_type"],
-            "wall_fenestration_ratio": compute_average_value(row["front_wall_fenestration_ratio"], row["front_wall_fenestration_ratio"],
-                                                                  row["front_wall_fenestration_ratio"], row["front_wall_fenestration_ratio"]),
-            "large_door_present": True if row["large_door_present"] == "yes" else False,
-            "large_door_opening_type": group_to_array([row["large_door_opening_type_front"], row["large_door_opening_type_front"],
-                                                       row["large_door_opening_type_front"], row["large_door_opening_type_front"]]),
-            "secondary_water_barrier": row["secondary_water_barrier"],
-            "overhang_length": row["overhang_length"],
-            "parapet_height": row["parapet_height_inches"],
-            "reroof_year": row["reroof_year"]
-        }
-        process_hurricane_harvey(housing_document, hazard_effect_document)
+def parse_hurricane_michael(row):
+    hazard_effect_document = {
+        "damage_state": row["status"],
+        "date": row["date"],
+        "assessment_type": row["assessment_type"],
+        "photo": row["all_photos"],
+        "photo_caption": row["all_photos_caption"],
+        "surveyor_notes": collect_notes([row["general_notes"], row["overall_damage_notes"],
+                                        row["structural_notes"], row["wind_damage_details"],
+                                        row["water_induced_damage_notes"]]),
+        "hazards_present": split_to_array_comma(row["hazards_present"]),
+        "wind_damage_rating": row["wind_damage_rating"],
+        "surge_damage_rating": row["surge_damage_rating"],
+        "rainwater_ingress_damage_rating": row["rainwater_ingress_damage_rating"],
+        "roof_structure_damage": row["roof_structure_damage_"],
+        "roof_substrate_damage": row["roof_substrate_damage"],
+        "roof_cover_damage": row["roof_cover_damage_"],
+        "wall_structure_damage": row["wall_structure_damage_"],
+        "wall_substrate_damage": row["wall_substrate_damage_"],
+        "building_envelope_damage": row["building_envelope_damage_"],
+        "wall_fenestration_damage": compute_average_value(row["front_wall_fenestration_damage"], row["left_wall_fenestration_damage"],
+                                                            row["right_wall_fenestration_damage"], row["back_wall_fenestration_damage"]),
+        "soffit_damage": row["soffit_damage"],
+        "fascia_damage": row["fascia_damage_"],
+        "stories_with_damage": row["stories_with_damage"],
+        "piles_missing_or_collapsed": row["_piles_missing_or_collapsed"],
+        "piles_leaning_or_broken": row["_piles_leaning_or_broken"],
+        "cause_of_foundation_damage": row["cause_of_foundation_damage"]
+    }
+    building_specific_document = {
+        "latitude": row['latitude'],
+        "longitude": row["longitude"],
+        "building_address": row["address_full"],
+        "building_type": row["building_type"],
+        "number_of_stories": row["number_of_stories"],
+        "understory_pct_of_building_footprint": row["understory_pct_of_building_footprint"],
+        "elevation_height": row["first_floor_elevation_feet"],
+        "year_built": convert_to_int(row["year_built"]),
+        "roof_shape": row["roof_shape"],
+        "roof_system": split_to_array(row["roof_system"]),
+        "roof_to_wall_attachment": row["r2wall_attachment"],
+        "roof_substrate_type": row["roof_substrate_type"],
+        "roof_cover": split_to_array(row["roof_cover"]),
+        "roof_slope": row["roof_slope"],
+        "main_wind_force_resisting_system": split_to_array(row["mwfrs"]),
+        "foundation_type": row["foundation_type"],
+        "wall_anchorage_type": row["wall_anchorage_type"],
+        "wall_structure": row["wall_structure"],
+        "wall_substrate": row["wall_substrate"],
+        "wall_cladding": row["wall_cladding"],
+        "soffit_type": row["soffit_type"],
+        "wall_fenestration_ratio": compute_average_value(row["front_wall_fenestration_ratio"], row["back_wall_fenestration_ratio"],
+                                                                row["left_wall_fenestration_ratio"], row["right_wall_fenestration_ratio"]),
+        "large_door_present": True if row["large_door_present"] == "yes" else False,
+        "large_door_opening_type": group_to_array([row["large_door_opening_type_front"], row["large_door_opening_type_back"],
+                                                    row["large_door_opening_type_left"], row["large_door_opening_type_right"]]),
+        "secondary_water_barrier": row["secondary_water_barrier"],
+        "overhang_length": row["overhang_length"],
+        "parapet_height": row["parapet_height_inches"],
+        "reroof_year": row["reroof_year"]
+    }
+    return hazard_effect_document, building_specific_document
 
 def compute_average_value(front, left, right, back):
     fenestration_ratio_sum, count = 0, 0
@@ -86,29 +82,34 @@ def compute_average_value(front, left, right, back):
         count += 1
     return math.nan if count == 0 else fenestration_ratio_sum / count
 
+# Helper function to convert float values to int. Checks for any NaN values.
+def convert_to_int(value):
+    if math.isnan(value): return value
+    return int(value)
+
 # Helper function for grouping multiple columns into an array.
 def group_to_array(arr):
     ret_arr = []
     for elem in arr:
-        if not math.isnan(elem): ret_arr.append(elem)
+        if not isinstance(elem, str) and math.isnan(elem): continue
+        ret_arr.append(elem)
     return ret_arr
 
 # Helper function to split array csv string into an actual array.
 # Ex. "Roof Diaphragm, wood","Wall Diaphragm, wood" -> ["Roof Diaphragm, wood", "Wall Diaphragm, wood"]
-def split_to_array(str):
-    return re.findall(r'"(.*?)"', str)
+def split_to_array(elem):
+    if not isinstance(elem, str) and math.isnan(elem): return elem
+    return re.findall(r'"(.*?)"', elem)
 
 # Helper function to split array csv string into an actual array by the comma.
 # Ex. "tree, wind" -> ["tree", "wind"]
-def split_to_array_comma(str):
-    return str.split(',')
+def split_to_array_comma(elem):
+    if not isinstance(elem, str) and math.isnan(elem): return elem
+    return elem.split(',')
 
 def collect_notes(arr):
     ret_str = ""
     for elem in arr:
-        if not math.isnan(elem): ret_str += elem
+        if not isinstance(elem, str) and math.isnan(elem): continue
+        ret_str += elem
     return math.nan if len(ret_str) == 0 else ret_str
-
-
-if __name__ == "__main__":
-    parse_hurricane_michael_csv_file("../Data/Hurricane_Michael/HM_D2D_Building.csv")
